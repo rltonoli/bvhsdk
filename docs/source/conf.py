25329d2d8db1e579b02f5d3a0932987034049ffc
rltonoli <rltonoli@gmail.com>
2023-09-07 16:10:19 -0300

    drop the use of myst_parser

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser",
     "sphinx.ext.duration",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
